b214d481297f8e69e766c23e183b3852599a038e
Luca Di Fazio <highliuk@gmail.com>
2024-11-20 18:35:35 +0100

    ♻️ Simplify stores

--- src/stores/chatListStore.ts
+++ src/stores/chatListStore.ts
@@ -1,29 +1,15 @@
 import { writable } from "svelte/store";
 import type { Chat } from "../types";
 
-const chatStore = () => {
-  const chatList: Chat[] = [
-    {
-      id: 0,
-      name: "Homer Simpson",
-      imgUrl: "/homer.jpg",
-    },
-    {
-      id: 1,
-      name: "Bart Simpson",
-      imgUrl: "/bart.jpg",
-    },
-  ];
-
-  const { subscribe, set, update } = writable(chatList);
-
-  return {
-    subscribe,
-    set,
-    addUser: (user: Chat) => update((data) => [...data, user]),
-    removeUser: (id: number) =>
-      update((data) => data.filter((u) => u.id !== id)),
-  };
-};
-
-export const chatListStore = chatStore();
+export const chatListStore = writable<Chat[]>([
+  {
+    id: 0,
+    name: "Homer Simpson",
+    imgUrl: "/homer.jpg",
+  },
+  {
+    id: 1,
+    name: "Bart Simpson",
+    imgUrl: "/bart.jpg",
+  },
+]);

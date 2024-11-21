ff3a3088ecfcdb429db63833c833ff07ed1db42c
Luca Di Fazio <highliuk@gmail.com>
2024-11-21 16:05:55 +0100

    ♻️ Inject Chats as App props

--- src/App.svelte
+++ src/App.svelte
@@ -4,74 +4,7 @@
 
   let chat: ChatType | null = null;
 
-  const chats: ChatType[] = [
-    {
-      id: 0,
-      name: "Homer Simpson",
-      messages: [
-        {
-          id: 0,
-          text: "My favorite quotes of Homer Simpson",
-          time: "3:14 PM",
-          received: true,
-        },
-        {
-          id: 1,
-          text: "Maybe, just once, someone will call me 'Sir', without adding 'you are making a scene'.",
-          time: "3:15 PM",
-          received: true,
-        },
-        {
-          id: 2,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: false,
-        },
-        {
-          id: 3,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: false,
-        },
-        {
-          id: 4,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: true,
-        },
-      ],
-    },
-    {
-      id: 1,
-      name: "Bart Simpson",
-      messages: [
-        {
-          id: 4,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: true,
-        },
-        {
-          id: 5,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: true,
-        },
-        {
-          id: 6,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: true,
-        },
-        {
-          id: 7,
-          text: "Doh!",
-          time: "3:16 PM",
-          received: false,
-        },
-      ],
-    },
-  ];
+  export let chats: ChatType[] = [];
 </script>
 
 <Phone>
--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,76 @@ import App from "./App.svelte";
 
 const app = mount(App, {
   target: document.getElementById("app")!,
+  props: {
+    chats: [
+      {
+        id: 0,
+        name: "Homer Simpson",
+        messages: [
+          {
+            id: 0,
+            text: "My favorite quotes of Homer Simpson",
+            time: "3:14 PM",
+            received: true,
+          },
+          {
+            id: 1,
+            text: "Maybe, just once, someone will call me 'Sir', without adding 'you are making a scene'.",
+            time: "3:15 PM",
+            received: true,
+          },
+          {
+            id: 2,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: false,
+          },
+          {
+            id: 3,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: false,
+          },
+          {
+            id: 4,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: true,
+          },
+        ],
+      },
+      {
+        id: 1,
+        name: "Bart Simpson",
+        messages: [
+          {
+            id: 4,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: true,
+          },
+          {
+            id: 5,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: true,
+          },
+          {
+            id: 6,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: true,
+          },
+          {
+            id: 7,
+            text: "Doh!",
+            time: "3:16 PM",
+            received: false,
+          },
+        ],
+      },
+    ],
+  },
 });
 
 export default app;
